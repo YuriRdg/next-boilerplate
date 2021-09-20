a0a7fa58fe534021360815fe76669cdc0dd18891
Yuri Rodrigues <yuri_a.rodrigues@hotmail.com>
2021-09-19 22:09:14 -0300

    feat(core): add themes, commitzen support and new plop templates

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -12,7 +12,22 @@ module.exports = function (plop) {
       {
         type: "add",
         path: "../src/components/{{pascalCase name}}/index.tsx",
-        templateFile: "templates/index.tsx.hbs"
+        templateFile: "templates/Component.tsx.hbs"
+      },
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/styles.ts",
+        templateFile: "templates/styles.ts.hbs"
+      },
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/stories.tsx",
+        templateFile: "templates/stories.tsx.hbs"
+      },
+      {
+        type: "add",
+        path: "../src/components/{{pascalCase name}}/test.tsx",
+        templateFile: "templates/test.tsx.hbs"
       }
     ]
   })
---
+++ src/styles/theme.ts
@@ -0,0 +1,45 @@
+export default {
+  border: {
+    radius: "0.4rem"
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    normal: 400,
+    bold: 700,
+    sizes: {
+      xxsmall: "1rem",
+      xsmall: "1.4rem",
+      small: "1.6rem",
+      medium: "1.8rem",
+      large: "2.1rem",
+      xlarge: "2.8rem",
+      xxlarge: "4.8rem"
+    }
+  },
+  colors: {
+    primary: "#FFD74F",
+    "purple-100": "#A68EEF",
+    "purple-200": "#7955E8",
+    "purple-300": "#744FF4",
+    "purple-400": "#6F48EC",
+    "purple-500": "#200E64",
+    white: "#fff"
+  },
+  spacings: {
+    xxsmall: "0.8rem",
+    xsmall: "1.6rem",
+    small: "2.4rem",
+    medium: "3.2rem",
+    large: "4.0rem",
+    xlarge: "4.8rem",
+    xxlarge: "5.6rem"
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  }
+}
