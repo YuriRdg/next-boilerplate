74c2379afe11793662b24c1905efcb565b90005b
Yuri Almeida Rodrigues <yuri_a.rodrigues@hotmail.com>
2022-01-20 09:11:00 -0300

    fix: fix nextjs update bug and prettier end of line

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,5 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext
-} from "next/document"
+import Document from "next/dist/pages/_document"
+import { Html, Head, Main, NextScript, DocumentContext } from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
